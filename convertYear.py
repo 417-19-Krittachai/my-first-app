422b949dec2afcc662b279364ab2d08e9d3fe7cc
417-19-Krittachai <56408@yupparaj.ac.th>
2026-08-05 11:46:04 +0700

    Update convertYear.py

--- convertYear.py
+++ convertYear.py
@@ -3,4 +3,4 @@ st.title("เเอปพลิเคชั่นเเปลงปี พ.ศ. 
 
 bh_year=st.number_input("กรอกปี พ.ศ. ที่ต้องการเเปลง",value=2569)
 ce_year=bh_year-543
-st.header(f"ปี ค.ศ. คือ : {ce_year"})  
+st.header(f"ปี ค.ศ. คือ : {ce_year}")  

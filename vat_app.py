54bf04217bf7d068f1d2893cc842063976653b13
417-19-Krittachai <56408@yupparaj.ac.th>
2026-08-26 11:29:27 +0700

    Update vat_app.py

--- vat_app.py
+++ vat_app.py
@@ -4,6 +4,6 @@ vat = price * 0.07
 st.header(f"• ภาษีมูลค่าเพิ่ม (VAT 7%): **{vat:.2f}** บาท")
 import streamlit as st
 price = st.number_input("กรอกราคาสินค้า (บาท):", value=0.0)
-net_price = price - vat
+net_price = 299 - 7.0
 st.write("นายกฤตชัย เเก้วอุ่น เลขที่ 19  ม.4/17")
 st.header(f"• ราคาสุทธิ: {net_price:.2f} บาท")

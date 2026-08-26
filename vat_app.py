cd51333a428b7d8df3a52567c45e328ff2d36ca5
417-19-Krittachai <56408@yupparaj.ac.th>
2026-08-26 11:42:36 +0700

    Update vat_app.py

--- vat_app.py
+++ vat_app.py
@@ -1,8 +1,8 @@
 import steamlit as st
 st.title("🛒แอปพลิเคชั่นคำนวณราคาสินค้ารวม VAT 7%")
-st.header(f"• ภาษีมูลค่าเพิ่ม (VAT 7%): **{vat:.2f}** บาท")
+price=st.number_input("200(บาท):",value=0.0)
 import streamlit as st
-price = st.number_input("กรอกราคาสินค้า (บาท):", value=0.0)
-net_price = price - vat
+price = st.number_input("35 (บาท):", value=0.0)
+net_price = 200 - 35
 st.write("นายกฤตชัย เเก้วอุ่น เลขที่ 19  ม.4/17")
 st.header(f"• ราคาสุทธิ: {net_price:.2f} บาท")

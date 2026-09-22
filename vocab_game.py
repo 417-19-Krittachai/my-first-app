3590d03ccbe486aea5c9bbf3a96fc703e2e1ebe0
417-19-Krittachai <56408@yupparaj.ac.th>
2026-09-22 22:15:20 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -5,7 +5,7 @@ st.title("@ เกมเติมศัพท์จับเวลา")
 
 # 1. กำหนดค่าเริ่มต้นใน session_state ถ้ายังไม่มี
 if "ans1_val" not in st.session_state:
-st. session_state.ans1_val = ""
+    st.session_state.ans1_val = ""
 if "ans2_val" not in st.session_state:
 st.session_state.ans2_val = ""
 

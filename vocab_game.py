6e60aec3d14ec13b7f8eeb24bd6eef1a947c9f6c
417-19-Krittachai <56408@yupparaj.ac.th>
2026-09-22 22:21:50 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -21,10 +21,11 @@ def reset_game():
 #
 @st.dialog("สรุปผลการเล่นเกม")
 def show_result_dialog(ans1, ans2):
-st.balloons() 
-score = 0
-u_ans1 = ans1.strip().lower()
-u_ans2 = ans2.strip().lower()
+    st.balloons() 
+    score = 0
+
+    u_ans1 = ans1.strip().lower()
+    u_ans2 = ans2.strip().lower()
 
 # ตรวจข้อ 1
 if u_ans1 == "apple":

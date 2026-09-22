5e73876dafe9ab5c251b6c954954517cc593e76e
417-19-Krittachai <56408@yupparaj.ac.th>
2026-09-22 21:39:19 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -11,7 +11,8 @@ st.session_state.ans2_val = ""
 
 # * ฟังก์ชันเคลียร์ค่าเมื่อกดปุ่มเริ่มใหม่
 def reset_game():
-st.session_state.ans1_val = "* # เคลียร์ค่าช่องข้อ 1
+st.session_state.ans2_val
+# เคลียร์ค่าช่องข้อ 1
 st.session_state.ans2_val = "" # เคลียร์ค่าช่องข้อ 2
 st.session_state.start = time.time() # เริ่มเวลาใหม่
 st.session_state.is_ended = False # ilø Dialog
